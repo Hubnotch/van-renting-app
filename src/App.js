5cacf0c17e97a7b5444b6370dfda57ec990fd5e2
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2023-03-18 11:12:56 +0100

    Added default imports and Outlet for the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
+import { Routes, Route,  } from "react-router-dom"
+import { About, Home, VanDetail, Vans, } from './pages'
+import {Layout }from './components'
 
 import "./server"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+      <Routes element={<Layout/>}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
-    </BrowserRouter>
   )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './About'
+export { default as Home } from './Home'
+export { default as VanDetail } from './VanDetail'
+export { default as Vans } from './Vans'
+
